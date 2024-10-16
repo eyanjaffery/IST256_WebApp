97eeda982f68d60c35c5e43990724553d2bea667
Eyan Jaffery <124312130+eyanjaffery@users.noreply.github.com>
2024-10-16 00:29:28 -0400

    The Bookshelf. - Images for books. - Books added. - Scroll to forms function added for pages that have form. - jQuery used for the auto scroll feature.

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,15 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+// scripts.js
+
+/// Check if the form exists on the page and scroll to it when the page loads
+$(document).ready(function() {
+    // Check if the "scrollUpForm" exists
+    if ($('#scrollUp').length) {
+        // Smooth scroll to the form section when the page loads
+        $('html, body').animate({
+            scrollTop: $('#scrollUp').offset().top
+        }, 200);  // Scroll duration in milliseconds
+    }
+});
